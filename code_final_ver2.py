05d2ecde4f180ce4aa22acd7163c90c86d02ad8f
Nguyễn Văn Quang <73304614+Quangvannguyenn@users.noreply.github.com>
2024-06-22 10:44:54 +0700

    Update code_final_ver2.py

--- code_final_ver2.py
+++ code_final_ver2.py
@@ -1,148 +1,149 @@
-import requests
-import pandas as pd
-import json
-from transformers import AutoTokenizer
-from transformers import RobertaForSequenceClassification
-import torch
-import numpy as np
 import streamlit as st
+import googleapiclient.discovery
+import pandas as pd
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler
+import numpy as np
 import plotly.graph_objects as go
 
+tokenizer = AutoTokenizer.from_pretrained("quanqnv19/hsd-model")
+model = AutoModelForSequenceClassification.from_pretrained("quanqnv19/hsd-model")
 device = 'cpu'
-MAX_LEN = 125
 
 
-tokenizer = AutoTokenizer.from_pretrained("quanqnv19/VN-Sentiment-Classification")
-model = RobertaForSequenceClassification.from_pretrained("quanqnv19/VN-Sentiment-Classification")
 
-def get_comment(comment):
-        # print(comment)
-        return {
-        # 'uid': comment['from']['id'],
-        # 'name': comment['from']['name'],
-        'time': comment['created_time'],
-        'message': comment['message'], 
-        'cmt_id': comment['id']
-    }
 
-def get_post(data):
-    return {
-        'id_post': data['id'],
-        'time': data['created_time'],
-        'message': data['message'], 
-    }
+def encoder_generator(sentences):
+    input_ids = []
+    attention_masks =[]
+    encoded_dict = tokenizer.encode_plus(sentences,
+                                             add_special_tokens=True,
+                                             max_length=20,
+                                             pad_to_max_length=True,
+                                             truncation = True,
+                                             return_attention_mask=True,
+                                             return_tensors='pt')
+    input_ids.append(encoded_dict['input_ids'])
+    attention_masks.append(encoded_dict['attention_mask'])
+    input_ids = torch.cat(input_ids,dim=0)
+    attention_masks = torch.cat(attention_masks,dim=0)
+    return input_ids,attention_masks
 
-def get_comments(post_id, long_term_accessToken):
-    url = f'https://graph.facebook.com/v19.0/{post_id}/comments?access_token={long_term_accessToken}&limit=1000'
-    response = requests.request("GET", url)
-    data = json.loads(response.text)
-    print(data)
-    excel_data = list(map(get_comment, data['data']))
-    df = pd.DataFrame(excel_data)
-    # df.to_excel('comments.xlsx', index=False)
-    return excel_data, df
-
-def get_feed(long_term_accessToken):
-    url = f'https://graph.facebook.com/v19.0/me/feed?access_token={long_term_accessToken}'
-    response = requests.request("GET", url)
-    data = json.loads(response.text)
-    feed_data = list(map(get_post, data['data']))
-    return feed_data
-
-def get_comments_in_post(data_post):
+def get_data_comment(input_id):
+    api_service_name = "youtube"
+    api_version = "v3"
+    DEVELOPER_KEY = "AIzaSyDbt-xdAOjDhJghQGVMxfbsSiSyCFJr1Jw"
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, developerKey=DEVELOPER_KEY)
+    request = youtube.commentThreads().list(
+        part="snippet",
+        videoId=input_id,
+        maxResults=1000
+    )
     comments = []
-    for item in data_post:
-        comments.append(item['message'])
-    return comments
+    response = request.execute()
+    for item in response['items']:
+        comment = item['snippet']['topLevelComment']['snippet']
+        public = item['snippet']['isPublic']
+        comments.append([
+            comment['authorDisplayName'],
+            comment['publishedAt'],
+            comment['likeCount'],
+            comment['textOriginal'],
+            public
+        ])
+    while (1 == 1):
+        try:
+            nextPageToken = response['nextPageToken']
+        except KeyError:
+            break
+        nextPageToken = response['nextPageToken']
+        nextRequest = youtube.commentThreads().list(part="snippet", videoId="-GJgqIJsTME", maxResults=1000, pageToken=nextPageToken)
+        response = nextRequest.execute()
+        for item in response['items']:
+            comment = item['snippet']['topLevelComment']['snippet']
+            public = item['snippet']['isPublic']
+            comments.append([
+                comment['authorDisplayName'],
+                comment['publishedAt'],
+                comment['likeCount'],
+                comment['textOriginal'],
+                public
+            ])
+    df = pd.DataFrame(comments, columns=['Author', 'Updated at', 'Like count', 'Comment','public'])
+    return df.drop('public', axis=1)
 
-def display_data(data_post, post):
-    labels = ['Tích cực','Tiêu cực', 'Trung lập']
-    # comments = get_comments_in_post(data_post)
-    pos, nev, neu, labels_pos, labels_neu, labels_neg = process_data(data_post)
-    print(pos+nev+neu)
-    # data_df = data_df.assign(labels = labels_eval)
-    # data_df = data_df.insert(1, "Evaluate", labels_eval, True)
-    data_df_pos = pd.DataFrame(labels_pos)
-    data_df_neu = pd.DataFrame(labels_neu)
-    data_df_neg = pd.DataFrame(labels_neg)
-    values = [pos, nev, neu]
+def process(data, list_clean, list_offensive, list_hate):
+    for i in range(len(data)):
+        input_ids, attention_masks = encoder_generator(data.iloc[i]['Comment'])
+        in_data = TensorDataset(input_ids, attention_masks)
+        in_data_loader = DataLoader(in_data, sampler=RandomSampler(in_data))
+        with torch.no_grad():
+            for batch in tqdm(in_data_loader):
+                b_input_ids = batch[0].to(device)
+                b_input_mask = batch[1].to(device)
+                embedded = model(b_input_ids,b_input_mask)[0]
+                predictions = np.argmax(embedded[0]).flatten()[0].item()
+                state = predictions
+                if state == 0:
+                    list_clean.append(data.iloc[i])
+                elif state == 1:
+                    list_offensive.append(data.iloc[i])
+                else:
+                    list_hate.append(data.iloc[i])
+
+
+# process(get_data_comment('b2-NB7UUBcU'))
+
+def display(data):
+    list_clean = []
+    list_offensive = []
+    list_hate = []
+    labels = ['CLEAN','OFFENSIVE', 'HATE']
+    process(data, list_clean, list_offensive, list_hate)
+    values = [len(list_clean), len(list_offensive), len(list_hate)]
     fig = go.Figure(data=[go.Pie(labels=labels, values=values, textinfo='label+percent', insidetextorientation='radial')])
     fig.update_layout(height=400, width=400)
     container = st.container(border=True)
     lef_col, right_col = container.columns(2)
     with lef_col:
-        st.subheader("Id bài viết: ")
-        st.write(post['id_post'])
-        st.markdown("---")
-        st.subheader("Nội dung:")
-        st.write(post['message'])
-        st.markdown("---")
         st.subheader("Kết quả phân loại bình luận:")
-        st.write("Tiêu cực: "+str(nev))
-        st.write("Tích cực: "+str(pos))
-        st.write("Trung lập: "+str(neu))
+        st.write("CLEAN: "+str(len(list_clean)))
+        st.download_button(
+            "Tệp bình luận CLEAN",
+            pd.DataFrame(list_clean).to_csv(index=False).encode('utf-8'),
+            "file.csv",
+            "text/csv",
+            key="1"
+        )
+        st.write("OFFENSIVE: "+str(len(list_offensive)))
+        st.download_button(
+            "Tệp bình luận OFFENSIVE",
+            pd.DataFrame(list_offensive).to_csv(index=False).encode('utf-8'),
+            "file.csv",
+            "text/csv",
+            key="2"
+        )
+        st.write("HATE: "+str(len(list_hate)))
+        st.download_button(
+            "Tệp bình luận HATE",
+            pd.DataFrame(list_hate).to_csv(index=False).encode('utf-8'),
+            "file.csv",
+            "text/csv",
+            key="3"
+        )
     with right_col:
         st.plotly_chart(fig, use_container_width=True)
-        st.download_button(
-            "Tệp bình luận đánh giá tích cực",
-            data_df_pos.to_csv(index=False).encode('utf-8'),
-            "file.csv",
-            "text/csv",
-            key=post['id_post']+"1"
-        )
-        st.download_button(
-            "Tệp bình luận đánh giá trung lập",
-            data_df_neu.to_csv(index=False).encode('utf-8'),
-            "file.csv",
-            "text/csv",
-            key=post['id_post']+"2"
-        )
-        st.download_button(
-            "Tệp bình luận đánh giá tiêu cực",
-            data_df_neg.to_csv(index=False).encode('utf-8'),
-            "file.csv",
-            "text/csv",
-            key=post['id_post']+"3"
-        )
-    print("ok")
-
-
-
-
-def process_data(comments):
-    labels_pos = []
-    labels_neg = []
-    labels_neu = []
-    neg, pos, neu = 0, 0, 0
-    for comment in comments:
-        # print(tokenizer.encode(sentence))
-        sentence = comment['message']
-        input_ids = torch.tensor([tokenizer.encode(sentence)])
-        with torch.no_grad():
-            out = model(input_ids)
-            # print(out.logits.softmax(dim=-1).tolist())
-            res = np.argmax(out[0], axis=1).flatten()[0].item()
-            if res == 0:
-                neg = neg + 1
-                labels_neg.append(comment)
-            elif res == 1:
-                pos = pos + 1
-                labels_pos.append(comment)
-            else:
-                neu = neu + 1
-                labels_neu.append(comment)
-    return pos, neg, neu, labels_pos, labels_neu, labels_neg
-
 
 if __name__ == '__main__':
-    container = st.container(border=True)
-    file = container.file_uploader("Upload Page access token file: ")
-    if file:
-        for token in file:
-            long_term_accessToken = token.decode("utf-8")
-        feeds = get_feed(long_term_accessToken)
-        for post in feeds:
-            data_comments, data_df = get_comments(post['id_post'], long_term_accessToken)
-            # print(data_comments)
-            display_data(data_comments, post)
-
+    st.markdown("<h2 style='text-align: center; color: #8C3819;'>Nhận dạng nội dung tiêu cực và xúc phạm PhoBERT-CNN</h2>", unsafe_allow_html=True)
+    input_id = st.text_input("Nhập vào URL Video YouTube")
+    if len(input_id)>2:
+        id_video = input_id.split("v=")[1]
+        data = get_data_comment(id_video)
+        display(data)
+        st.dataframe(data)
